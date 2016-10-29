2f5f9bfee83828224143f6cb2d21b408e3b6ce23
PigeonZombie <alicia.orchidee@gmail.com>
2016-10-29 09:18:09 -0400

    Tutorial finished in june 2016

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+    private int floorMask;
+    private float camRayLength = 100f;
+
+
+    // Similar to Start(), but gets called wether the script is active or not 
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Physics updates
+    void FixedUpdate()
+    {
+        // Raw = -1, 0 or 1, nothing in between
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+        Turning();
+        Animating(horizontal, vertical);
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        movement.Set(horizontal, 0f, vertical);
+        //To avoid going faster diagonally 
+        movement = movement.normalized*speed*Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        // If the ray hit something
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+}
